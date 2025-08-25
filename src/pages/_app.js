a8c1121faafa123cccf77dad8641a8f32768bcdc
afreenshaikhst23i <46025523+afreenshaikhst23i@users.noreply.github.com>
2025-08-26 00:25:40 +0530

    google analytics setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import ScrollToTop from "@/components/common/ScrollToTop";
+import Script from "next/script";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps, router }) {
@@ -11,12 +12,24 @@ export default function App({ Component, pageProps, router }) {
 
     return (
         <>
-        <Header></Header>
-        <main className="content-main">
-            <Component {...pageProps} />
-        </main>
-        <ScrollToTop />
-        <Footer></Footer>
+            <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-PMME4E64CH`} />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-PMME4E64CH', {
+                    page_path: window.location.pathname,
+                });
+                `}
+            </Script>
+            <Header></Header>
+            <main className="content-main">
+                <Component {...pageProps} />
+            </main>
+            <ScrollToTop />
+            <Footer></Footer>
         </>
     );
 }
